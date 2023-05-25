c92b7cbd419c33008cd8fc9797057eee3329e2bf
Rolex Omondi <rolexomondi46@gmail.com>
2023-05-25 16:28:09 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_numbers = []
+    for i in range(length):
+        if i == 0 or i == 1:
+            fibonacci_numbers.append(i)
+        else:
+            fibonacci_numbers.append(fibonacci_numbers[i-1] + fibonacci_numbers[i-2])
+    print(fibonacci_numbers)
